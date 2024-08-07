49e2d7cbc929b4fc6bcbb63d80c48799c5b572db
samualhalder <samual.halder2020@gmail.com>
2024-08-08 01:20:07 +0530

    chaged ui for add question section

---
+++ client/src/components/AddQst.tsx
@@ -0,0 +1,79 @@
+import { Button, HR, TextInput } from "flowbite-react";
+import { FaMinus, FaEdit, FaTrash, FaPlus } from "react-icons/fa";
+import { qs } from "../pages/DashCreatePost";
+import { useState } from "react";
+
+function AddQst({
+  params,
+}: {
+  params: {
+    qsArrey: qs[];
+    setQsArrey: (value: qs[]) => void;
+  };
+}) {
+  const [question, setQuestion] = useState<string>("");
+  const [isEditing, setIsEditing] = useState<string | null>(null);
+
+  const addQuestion = () => {
+    if (isEditing) {
+      const newArrye: qs[] = params.qsArrey.map((elm) =>
+        elm.id === isEditing ? { ...elm, question: question } : elm
+      );
+      params.setQsArrey([...newArrye]);
+      setIsEditing(null);
+    } else {
+      const newQst: qs = {
+        id: new Date().toLocaleString(),
+        question: question,
+      };
+      params.setQsArrey([...params.qsArrey, newQst]);
+    }
+    setQuestion("");
+  };
+  const handleEdit = (id: string) => {
+    setIsEditing(id);
+    const editedQuestion = params.qsArrey.filter((qs) => qs.id === id);
+    setQuestion(editedQuestion[0].question);
+  };
+  const handleDelete = (id: string) => {
+    const index = params.qsArrey.findIndex((elm) => elm.id === id);
+    params.qsArrey.splice(index, 1);
+    params.setQsArrey([...params.qsArrey]);
+  };
+  return (
+    <div className="flex flex-col w-full border-2 border-cyan-400 p-3">
+      <div className="w-full flex justify-between">
+        <TextInput
+          className="w-[90%]"
+          placeholder="Ask a question"
+          value={question}
+          onChange={(e) => setQuestion(e.target.value)}
+        />
+        <Button className="flex items-center" onClick={addQuestion}>
+          {!isEditing ? <FaPlus /> : "save"}
+        </Button>
+      </div>
+      <HR />
+      <div>
+        {params.qsArrey.map((qs, ind) => (
+          <div
+            key={ind}
+            className="text-xl flex justify-between border-2 border-cyan-200 p-4 m-2 rounded-3xl"
+          >
+            <p>{qs.question}</p>
+            <div className="flex gap-2 ">
+              <Button color={"red"} onClick={() => handleDelete(qs.id)}>
+                <FaTrash />
+              </Button>
+              <Button color={"yellow"} onClick={() => handleEdit(qs.id)}>
+                <FaEdit />
+              </Button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default AddQst;
--- client/src/pages/DashCreatePost.tsx
+++ client/src/pages/DashCreatePost.tsx
@@ -1,9 +1,15 @@
 import { Button, FileInput, Label, Textarea, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { FaPlus, FaMinus, FaQuestionCircle } from "react-icons/fa";
-import { ImCross } from "react-icons/im";
+import AddQst from "../components/AddQst";
+
+export type qs = {
+  id: string;
+  question: string;
+};
+
 function DashCreatePost() {
-  const [qsCount, setQsCount] = useState<number>(0);
+  const [qsArrey, setQsArrey] = useState<qs[]>([]);
   return (
     <div className="">
       <div className="flex justify-center items-center text-center">
@@ -38,35 +44,12 @@ function DashCreatePost() {
               helperText="you can upload a picture related to the feedback topic. (photo size must be under 2MB.)"
             />
           </div>
-          {qsCount > 0 &&
-            [...Array(qsCount)].map((x, i) => (
-              <div>
-                <div className="mb-2 block">
-                  <Label htmlFor="base" value={`question ${i + 1}`} />
-                </div>
-                <div className="">
-                  <TextInput
-                    id="base"
-                    className="inline"
-                    icon={FaQuestionCircle}
-                  />
-                </div>
-              </div>
-            ))}
-          <div className="flex justify-center items-center m-5 gap-2">
-            <Button
-              onClick={() => setQsCount((pre) => (pre < 5 ? pre + 1 : pre))}
-              className={`${qsCount >= 5 ? " hidden" : ""}`}
-            >
-              Add question <FaPlus size={20} className="mx-3" />
-            </Button>
-            <Button
-              onClick={() => setQsCount((pre) => (pre > 0 ? pre - 1 : pre))}
-              color={"red"}
-              className={`${qsCount == 0 ? " hidden" : ""}`}
-            >
-              Remove question <FaMinus size={20} className="mx-3" />
-            </Button>
+          <h1 className="flex justify-center mt-4 text-lg">
+            ask some question for your feedback form
+          </h1>
+          {/* Add question section */}
+          <div className="mt-10">
+            <AddQst params={{ qsArrey, setQsArrey }} />
           </div>
         </form>
       </div>
